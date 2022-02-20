05cc71478dac89f2e70f59f56e9adfa9cd1a70a5
chul0721 <chul070721@naver.com>
2022-02-20 23:54:08 +0900

    feat: project settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ChartController } from './chart/chart.controller'
+import { ChartModule } from './chart/chart.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [ChartModule],
+  controllers: [ChartController],
+  providers: []
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  )
   await app.listen(3000, '0.0.0.0')
 }
 bootstrap()
